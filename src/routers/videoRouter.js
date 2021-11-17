c05ffc96f65522e6455b95513de8120d19673aa6
Ju-yeop <dhwnduq12@naver.com>
2021-11-17 20:09:22 +0900

    FakeDB

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
-import {see, edit, upload, deleteVideo} from "../controllers/videoController";
+import {watch, getedit, postedit, getUpload, postUpload} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
-videoRouter.get("/:id(\\d+)", see);
-videoRouter.get("/:id(\\d+)/edit", edit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+
+videoRouter.get("/:id(\\d+)", watch);
+videoRouter.route("/:id(\\d+)/edit").get(getedit).post(postedit);
+videoRouter.get("/upload", getUpload);
+videoRouter.post("/upload", postUpload);
 
 export default videoRouter;
