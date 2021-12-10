993051772d648d7dc7fd7af345f9b14df2684ac6
Ju-yeop <dhwnduq12@naver.com>
2021-12-10 21:24:47 +0900

    comment

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,6 @@
 import Video from "../models/Video";
 import User from "../models/User";
+import Comment from "../models/Comment";
 
 export const home = async (req, res) => {
     const videos = await Video.find({})
@@ -9,7 +10,7 @@ export const home = async (req, res) => {
 }
 export const watch = async (req, res) => {
     const id = req.params.id;
-    const video = await Video.findById(id).populate("owner");
+    const video = await Video.findById(id).populate("owner").populate("comments");
     if (!video){
         return res.render("404", {pageTitle: "Video not found."});
     }
@@ -114,4 +115,20 @@ export const registerView = async(req, res) => {
     video.meta.views = video.meta.views + 1;
     await video.save();
     return res.sendStatus(200);
-}
+};
+
+export const createComment = async (req, res) => {
+    const { session: {user}, body: {text}, params: {id}} = req;
+    const video = await Video.findById(id);
+    if(!video){
+        return res.sendStatus(404);
+    } 
+    const comment = await Comment.create({
+        text,
+        owner: user._id,
+        video:id
+    });
+    video.comments.push(comment._id);
+    video.save();
+    return res.sendStatus(201);
+};
