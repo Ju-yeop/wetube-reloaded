b94d8de121b5205bbc6bbeffe60565b87bddb99d
Ju-yeop <dhwnduq12@naver.com>
2021-12-19 06:26:19 +0900

    upload

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -59,6 +59,8 @@ export const postUpload = async (req, res) => {
     const {video, thumb}= req.files;
     const {title, description, hashtags} = req.body;
     const isHeroku = process.env.NODE_ENV === "production";
+    console.log(video);
+    console.log(thumb);
     try{
         const newVideo = await Video.create({
             title,
