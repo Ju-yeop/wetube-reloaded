31feb3bb2d838e915f19118335faa519c5699d17
Ju-yeop <dhwnduq12@naver.com>
2021-12-19 03:15:39 +0900

    crossorigin

--- src/server.js
+++ src/server.js
@@ -29,7 +29,6 @@ app.use(session({
 app.use(flash());
 app.use((req, res, next) => {
     res.header("Cross-Origin-Embedder-Policy", "require-corp");
-    res.header("Cross-Origin-Opener-Policy", "same-origin");
     next();
     });
 app.use(localsMiddleware);
