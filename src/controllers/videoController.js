0a6e73d5dddf0f9d29ba4f6c6b1056ac6744fc06
Ju-yeop <dhwnduq12@naver.com>
2021-12-19 04:40:29 +0900

    videos

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -6,6 +6,7 @@ export const home = async (req, res) => {
     const videos = await Video.find({})
     .sort({ createdAt: "desc" })
     .populate("owner");
+    console.log(videos);
     return res.render("home", {pageTitle : "Home.", videos});
 }
 export const watch = async (req, res) => {
